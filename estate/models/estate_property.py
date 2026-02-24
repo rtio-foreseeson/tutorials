3dfe9bcfe81fe4536a52e20334951d40198b4eaf
RT <ryan.tio@foreseeson.com>
2026-02-24 14:47:14 -0800

    add fields

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,21 +1,30 @@
 from odoo import fields, models
 
+
 class EstateProperty(models.Model):
-    _name = "Estate_Property"
-    _description = "Estate Property Model"
+    _name = "estate.property"
+    _description = "Estate Property"
     # _order = "sequence"
 
-    # name = fields.Char("Property Name", required=True, translate=True)
-    # property_type = fields.Char("Property Type", required=True)
-    # postcode = fields.Char("Postcode", required=True)
-    # available_from = fields.Date("Available From", required=False)
-    # expected_price = fields.Integer("Expected Price", required=True)
-    # best_offer = fields.Integer("Best Offer", required=False)
-    # selling_proce = fields.Integer("Selling Price", required=False)
-    # description = fields.Char("Description", required=False)
-    # bedrooms = fields.Integer("Bedrooms", required=True)
-    # living_area = fields.Integer("Living Area (sqft)", required=True)
-    # facades = fields.Integer("Facades", required=True)
-    # has_garage = fields.Boolean("Garage", required=True)
-    # has_garden = fields.Boolean("Garden", required=True)
-    # total_area = fields.Integer("Total Area (sqm)", required=True)
+    name = fields.Char("Property Name", required=True)
+    description = fields.Char("Description")
+    postcode = fields.Char("Postcode")
+    date_available = fields.Date("Available From")
+    expected_price = fields.Float("Expected Price", required=True)
+    best_offer = fields.Float("Best Offer")
+    selling_proce = fields.Float("Selling Price")
+    bedrooms = fields.Integer("Bedrooms")
+    living_area = fields.Integer("Living Area (sqft)")
+    facades = fields.Integer("Facades")
+    has_garage = fields.Boolean("Garage")
+    has_garden = fields.Boolean("Garden")
+    garden_area = fields.Integer("Garden Area")
+    garden_orientation = fields.Selection(
+        string="Garden Orientation",
+        selection=[
+            ("north", "North"),
+            ("east", "East"),
+            ("south", "South"),
+            ("west", "West"),
+        ],
+    )
