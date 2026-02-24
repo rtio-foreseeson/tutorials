32b6df7e6abd8516cfe5c3cc4c6c17f6f0953405
RT <ryan.tio@foreseeson.com>
2026-02-24 14:21:51 -0800

    create basic module

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Estate',
+    'version': '1.0',
+    'category' : 'Tutorials/Estate',
+    'summary': 'Estate Test For the Tutorial',
+    'depends': [
+        'base_setup'
+    ],
+    'application': True,
+}
