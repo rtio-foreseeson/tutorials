66062d1d881a981ea0b789793e411ab9d172ed8e
RT <ryan.tio@foreseeson.com>
2026-02-24 14:22:05 -0800

    create minimal model (with extra fields)

---
+++ estate/models/estate_property.py
@@ -0,0 +1,21 @@
+from odoo import fields, models
+
+class EstateProperty(models.Model):
+    _name = "Estate_Property"
+    _description = "Estate Property Model"
+    # _order = "sequence"
+
+    # name = fields.Char("Property Name", required=True, translate=True)
+    # property_type = fields.Char("Property Type", required=True)
+    # postcode = fields.Char("Postcode", required=True)
+    # available_from = fields.Date("Available From", required=False)
+    # expected_price = fields.Integer("Expected Price", required=True)
+    # best_offer = fields.Integer("Best Offer", required=False)
+    # selling_proce = fields.Integer("Selling Price", required=False)
+    # description = fields.Char("Description", required=False)
+    # bedrooms = fields.Integer("Bedrooms", required=True)
+    # living_area = fields.Integer("Living Area (sqft)", required=True)
+    # facades = fields.Integer("Facades", required=True)
+    # has_garage = fields.Boolean("Garage", required=True)
+    # has_garden = fields.Boolean("Garden", required=True)
+    # total_area = fields.Integer("Total Area (sqm)", required=True)
