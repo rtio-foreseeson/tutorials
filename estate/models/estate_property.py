5f68fcdf7ed87ad3ecf5890d12e59f827c00fcf7
RT <ryan.tio@foreseeson.com>
2026-02-24 22:32:17 -0800

    update defaults

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import fields, models
+from datetime import timedelta
 
 
 class EstateProperty(models.Model):
@@ -9,11 +10,13 @@ class EstateProperty(models.Model):
     name = fields.Char("Property Name", required=True)
     description = fields.Char("Description")
     postcode = fields.Char("Postcode")
-    date_available = fields.Date("Available From")
-    expected_price = fields.Float("Expected Price", required=True)
-    best_offer = fields.Float("Best Offer")
-    selling_proce = fields.Float("Selling Price")
-    bedrooms = fields.Integer("Bedrooms")
+    date_available = fields.Date(
+        "Available From", copy=False, default=fields.Date.today() + timedelta(days=90)
+    )
+    expected_price = fields.Float("Expected Price")
+    best_offer = fields.Float("Best Offer", copy=False)
+    selling_price = fields.Float("Selling Price", readonly=True, copy=False)
+    bedrooms = fields.Integer("Bedrooms", default=2)
     living_area = fields.Integer("Living Area (sqft)")
     facades = fields.Integer("Facades")
     has_garage = fields.Boolean("Garage")
