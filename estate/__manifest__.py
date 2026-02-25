c95b686f66b80b37a9c01aff27a847d2ff40cbf2
RT <ryan.tio@foreseeson.com>
2026-02-24 16:05:12 -0800

    security

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'Estate',
-    'version': '1.0',
-    'category' : 'Tutorials/Estate',
-    'summary': 'Estate Test For the Tutorial',
-    'depends': [
-        'base_setup'
-    ],
-    'data': [],
-    'installable': True,
-    'application': True,
+    "name": "Estate",
+    "version": "1.0",
+    "category": "Tutorials/Estate",
+    "summary": "Estate Test For the Tutorial",
+    "depends": ["base_setup"],
+    "data": ["security/ir.model.access.csv", "views/estate_property_views.xml"],
+    "installable": True,
+    "application": True,
 }
