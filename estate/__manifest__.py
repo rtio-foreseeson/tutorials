39da39098e0e5d3f5bf9c65fe61c881bf604ac37
RT <ryan.tio@foreseeson.com>
2026-02-26 10:09:08 -0800

    offer

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -11,6 +11,7 @@
         "views/estate_property_views.xml",
         "views/estate_property_type_view.xml",
         "views/estate_property_tag_view.xml",
+        "views/estate_property_offer_view.xml",
         "views/estate_menus.xml",
     ],
     "installable": True,
