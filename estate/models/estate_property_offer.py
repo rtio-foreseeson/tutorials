7f1c0e9ce9923c03684f1ee59665a9e674fc6655
RT <ryan.tio@foreseeson.com>
2026-02-26 14:01:43 -0800

    accept and reject offers

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,10 +1,23 @@
+from __future__ import annotations
 from odoo import api, fields, models
 from datetime import timedelta
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .estate_property import EstateProperty
 
 
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
     _description = "Model for an offer made on a property"
+
+    _sql_constraints = [
+        (
+            "check_offer_price_positive",
+            "CHECK(price > 0)",
+            "Offer price must be positive.",
+        )
+    ]
 
     price = fields.Float("Price")
     status = fields.Selection(
@@ -17,7 +30,9 @@ class EstatePropertyOffer(models.Model):
         string="Deadline Date",
     )
     partner_id = fields.Many2one("res.partner", string="Buyer", required=True)
-    property_id = fields.Many2one("estate.property", string="Property", required=True)
+    property_id: EstateProperty = fields.Many2one(
+        "estate.property", string="Property", required=True
+    )
 
     @api.depends("validity")
     def _compute_deadline(self):
@@ -40,3 +55,16 @@ class EstatePropertyOffer(models.Model):
             else:
                 delta = record.deadline_date - fields.Date.today()
                 record.validity = delta.days
+
+    def accept_offer(self):
+        for record in self:
+            if record.property_id:
+                record.property_id.selling_price = self.price
+                record.property_id.buyer_id = self.partner_id
+                record.status = "accepted"
+        return True
+
+    def reject_offer(self):
+        for record in self:
+            record.status = "refused"
+        return True
