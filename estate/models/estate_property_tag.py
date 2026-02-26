208c060bee4e5c8ae427b4d8763fa920a9a99343
RT <ryan.tio@foreseeson.com>
2026-02-26 10:08:43 -0800

    add missing descriptions

--- estate/models/estate_property_tag.py
+++ estate/models/estate_property_tag.py
@@ -3,5 +3,6 @@ from odoo import fields, models
 
 class EstatePropertyTag(models.Model):
     _name = "estate.property.tag"
+    _description = "A tag describing a property e.g. Cozy, Renovated"
 
     name = fields.Char("Name", required=True)
--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -3,5 +3,6 @@ from odoo import fields, models
 
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
+    _description = "A type for a property (e.g. Apartment, House)"
 
     name = fields.Char("Name", required=True)
